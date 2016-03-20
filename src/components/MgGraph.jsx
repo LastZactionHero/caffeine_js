78771b2eec171e76bce64320f7d70b241238db10
Zach Dicklin <zdicklin@gmail.com>
2016-03-20 16:43:59 -0600

    Resize chart on window change

--- src/components/MgGraph.jsx
+++ src/components/MgGraph.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 var LineChart = require('react-chartjs').Line;
 
 var MgGraph = React.createClass({
+  getInitialState: function(){
+    return {
+      chartOptions: {
+        responsive: true
+      }
+    }
+  },
+  
   render(){
     var chart = null;
     if(this.props.data){
-      chart = <LineChart data={this.props.data} options='' className='mg-graph' redraw/>
+      chart = <LineChart data={this.props.data} options={this.state.chartOptions} className='mg-graph' redraw/>
     } else {
       chart = <h2>Loading...</h2>
     }
